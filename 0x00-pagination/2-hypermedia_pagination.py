0992989bd8fa392999efe31f93fefbc925f2af9f
Selomon1 <soya.vandy1950@gmail.com>
2024-03-29 21:31:47 +0300

    Pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -5,7 +5,7 @@ module containing a Server class for pagination of a dataset.
 
 import csv
 import math
-from typing import Tuple, List
+from typing import Tuple, List, Union, Optional, Dict
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -49,3 +49,28 @@ class Server:
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
         return dataset[start_index:end_index]
+
+    def get_hyper(
+        self,
+        page: int = 1,
+        page_size: int = 10
+    ) -> Dict[str, Union[int, List[List[str]], Optional[int]]]:
+        """
+        Returns hypermedia pagination details.
+        Args:
+            page (int): the current page number
+            page_size (int): the number of items per page
+        """
+        page_data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
